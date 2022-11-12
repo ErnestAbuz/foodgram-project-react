f8df7140666758fbaa251e9943d5d807f3c72e9e
Эрнест Абузаров <ernest.1998@mail.ru>
2022-11-13 02:25:07 +0500

    1

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -65,8 +65,8 @@ WSGI_APPLICATION = 'foodgram.wsgi.application'
 
 DATABASES = {
     'default': {
-        #'ENGINE': 'django.db.backends.sqlite3',
-        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         'ENGINE': os.getenv('DB_ENGINE'),
         'NAME': os.getenv('DB_NAME'),
         'USER': os.getenv('POSTGRES_USER'),
