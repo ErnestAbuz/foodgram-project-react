973bd238aa00fb5dba35f034cfad94646a1a7309
Эрнест Абузаров <ernest.1998@mail.ru>
2022-11-16 21:35:26 +0500

    2

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -82,7 +82,6 @@ class Recipe(models.Model):
     text = models.TextField(verbose_name='Текст')
     ingredients = models.ManyToManyField(
         IngredientsAmount,
-        through='IngredientsAmount',
         related_name='ingredients',
         verbose_name='Ингредиенты'
     )
