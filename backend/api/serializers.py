cced55da405e993c4851d68b1af284f310806d0c
Эрнест Абузаров <ernest.1998@mail.ru>
2022-11-21 23:46:34 +0500

    2

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -28,14 +28,22 @@ class IngredientSerializer(serializers.ModelSerializer):
 class IngredientsAmountSerializer(serializers.ModelSerializer):
     """Класс количества ингредиентов."""
     id = serializers.ReadOnlyField(source='ingredient.id')
-    name = serializers.ReadOnlyField(source='ingredient.name')
-    measurement_unit = serializers.ReadOnlyField(
+    name = serializers.SerializerMethodField(source='ingredient.name')
+    measurement_unit = serializers.SerializerMethodField(
         source='ingredient.measurement_unit'
     )
 
     class Meta:
         model = IngredientsAmount
         fields = ('id', 'name', 'measurement_unit', 'amount')
+
+    def get_name(self, value):
+        name = value.ingredient.name
+        return name
+
+    def get_measurement_unit(self, value):
+        measurement_unit = value.ingredient.measurement_unit
+        return measurement_unit
 
 
 class TagSerializer(serializers.ModelSerializer):
