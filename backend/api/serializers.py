83dc9124ecf3114c7f9f855c4293cd93025bd366
Эрнест Абузаров <ernest.1998@mail.ru>
2022-11-22 19:31:27 +0500

    2

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -62,7 +62,7 @@ class RecipeSerializer(serializers.ModelSerializer):
                   'cooking_time',)
 
     def get_ingredients(self, obj):
-        ingredients = IngredientsAmount.objects.filter(ingredient=obj)
+        ingredients = IngredientsAmount.objects.filter(recipe=obj)
         return IngredientsAmountSerializer(ingredients).data
 
     def get_author(self, value):
