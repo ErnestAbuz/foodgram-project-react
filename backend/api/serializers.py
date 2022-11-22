564b1f8ec6cf965cea64f068df4e49f42e52a87d
Эрнест Абузаров <ernest.1998@mail.ru>
2022-11-22 21:38:02 +0500

    2

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -3,7 +3,7 @@ import base64
 from django.conf import settings
 from django.core.files.base import ContentFile
 from django.shortcuts import get_object_or_404
-from django.db.models import F
+
 from recipes.models import (Favorite, Ingredient, IngredientsAmount, Recipe,
                             ShoppingCart, Tag)
 from rest_framework import serializers
