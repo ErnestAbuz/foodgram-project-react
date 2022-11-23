52bc42c568a7dada07ffa30eee5b093e8ccc5ea7
Эрнест Абузаров <ernest.1998@mail.ru>
2022-11-23 13:51:28 +0500

    2

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -114,8 +114,10 @@ class RecipeCreateSerializer(serializers.ModelSerializer):
             )
         ingredient_list = []
         for ingredient_item in ingredients:
-            ingredient_obj = get_object_or_404(Ingredient,
-                                               ingredient=ingredient_item['id'])
+            ingredient_obj = get_object_or_404(
+                Ingredient,
+                ingredient=ingredient_item['id']
+            )
             if ingredient_obj in ingredient_list:
                 raise serializers.ValidationError(
                     'Этот ингредиент уже добавлен'
