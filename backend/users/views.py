d68b57f824183a232484c578c3de380182a40eda
Эрнест Абузаров <ernest.1998@mail.ru>
2022-11-24 17:24:00 +0500

    2

--- backend/users/views.py
+++ backend/users/views.py
@@ -4,7 +4,8 @@ from rest_framework import status
 from rest_framework.decorators import action
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
-from api.pagination import ForPageNumberPagination, SubsRecipeNumberPagination
+from api.pagination import ForPageNumberPagination
+from rest_framework.pagination import LimitOffsetPagination
 from users.models import Subscription, User
 from users.serializers import (CustomUserSerializer, SubscriptionSerializer,
                                UserActionGetSerializer)
@@ -27,7 +28,7 @@ class CustomUserViewSet(UserViewSet):
             permission_classes=[IsAuthenticated])
     def subscriptions(self, request):
         authors = User.objects.filter(author__user=request.user)
-        paginator = SubsRecipeNumberPagination()
+        paginator = LimitOffsetPagination()
         result_pages = paginator.paginate_queryset(queryset=authors,
                                                    request=request)
         context = {'request': self.request}
